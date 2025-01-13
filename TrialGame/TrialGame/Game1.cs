fcd522260b0a6ae326c375f53d285dae3228f14c
artless <jayanlas00@gmail.com>
2025-01-12 21:49:51 -0500

    init commit

---
+++ TrialGame/TrialGame/Game1.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Diagnostics;
+
+namespace TrialGame
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        PlayerController playerController;
+        PlayerController enemyController;
+        Texture2D _texture;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            //_texture = Content.Load<Texture2D>("playership");
+            playerController = new PlayerController();
+            playerController.UnitTexture = Content.Load<Texture2D>("playership");
+            
+            enemyController = new PlayerController();
+            enemyController.UnitTexture = Content.Load<Texture2D>("playership");
+            enemyController.UnitRect = new Rectangle(100, 300, 128, 128);
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+                
+            playerController.MovePlayer();
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+
+            }
+
+
+            if (playerController.UnitRect.Intersects(enemyController.UnitRect))
+            {
+                Debug.WriteLine("intersection");
+            }
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
+
+            //_spriteBatch.Draw(_texture, new Rectangle(100, 100, 100, 200), Color.White);
+            _spriteBatch.Draw(playerController.UnitTexture, playerController.UnitRect, Color.White);
+            _spriteBatch.Draw(enemyController.UnitTexture, enemyController.UnitRect, Color.Red);
+            _spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ TrialGame/TrialGame/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrialGame
+{
+    internal class PlayerController
+    {
+        Texture2D unitTexture;
+        Rectangle unitRect;
+
+        public PlayerController() 
+        { 
+            unitTexture = null;
+            unitRect = new Rectangle(0, 0, 128, 128);
+        }
+
+        public Rectangle UnitRect
+        {
+            get { return unitRect; }
+            set { unitRect = value; }
+        }
+        public Texture2D UnitTexture
+        {
+            get { return unitTexture; }
+            set { unitTexture = value; }
+        }
+
+        public void MovePlayer()
+        {
+
+            int xCoord = this.UnitRect.X;
+            int yCoord = this.UnitRect.Y;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.S) || Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                yCoord += 2;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.D) || Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                xCoord += 2;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.W) || Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                yCoord -= 2;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.A) || Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                xCoord -= 2;
+            }
+            this.UnitRect = new Rectangle(xCoord, yCoord, 128, 128);
+
+            //playerController.UnitRect.Offset(xCoord+1, yCoord);
+            Debug.WriteLine("X: " + xCoord + ", Y: " + yCoord);
+
+        }
+    }
+}
